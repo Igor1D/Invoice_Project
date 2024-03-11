7c0ec02731a20577c3fd7abead3f313036a23768
Igor1D <43789550+Igor1D@users.noreply.github.com>
2024-03-11 21:40:03 +0100

    Jira items: KAN-17

--- src/AllInvoices.jsx
+++ src/AllInvoices.jsx
@@ -4,12 +4,15 @@ import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import IconButton from "@mui/material/IconButton";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { UseInvoicesContext } from "./Utils/InvoicesContextProvider.jsx";
+import Chip from "@mui/material/Chip";
+import CircleIcon from "@mui/icons-material/Circle.js";
 
 function AllInvoices() {
   const { invoices, setInvoices } = UseInvoicesContext();
   console.log(invoices);
+
   return (
     <div className="invoices-container">
       {invoices ? (
@@ -23,8 +26,32 @@ function AllInvoices() {
               </div>
               <div className="invoice-rightSide-div">
                 <p className="invoice-info">${invoice.total}</p>
+
                 <div className="invoice-status">
-                  <p>Paid</p>
+                  <Chip
+                    label={
+                      invoice.status.charAt(0).toUpperCase() +
+                      invoice.status.slice(1)
+                    }
+                    style={{
+                      backgroundColor: "#2B2736",
+                      color: "#FF8F00",
+                      padding: "15px",
+                      paddingTop: "25px",
+                      paddingBottom: "25px",
+                      fontSize: "18px",
+                      fontWeight: 550,
+                      borderRadius: "9px",
+                    }}
+                    icon={
+                      <CircleIcon
+                        style={{
+                          fontSize: "small",
+                          color: "#FF8F00",
+                        }}
+                      />
+                    }
+                  />
                 </div>
                 <Link
                   to={`/invoice/${invoice.id}`}
